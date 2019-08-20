2e4078131b003df2a3fa6b28332f839886c1acbd
Elcid Aaron Pangilinan <elcidaaron@gmail.com>
2019-08-20 15:19:53 +0800

    First commit. file_rename.py and compressed.

---
+++ file_rename.py
@@ -0,0 +1,16 @@
+#Command line interface setup
+import argparse
+parser = argparse.ArgumentParser(description='Copies the file with a different name.')
+parser.add_argument('infile', nargs='?', help='the name of the input file')
+parser.add_argument('outfile', nargs='?', help='the new name of the file')
+args = parser.parse_args()
+
+#Get contents of infile
+file_stream = open(args.infile, "r")
+contents = file_stream.read()
+file_stream.close()
+
+#Create the new outfile
+file_stream = open(args.outfile, "w+")
+file_stream.write(contents)
+file_stream.close()
