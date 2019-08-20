01221d549826b2e1af2ff5aab9b060c8dbfd7163
Elcid Aaron Pangilinan <elcidaaron@gmail.com>
2019-08-20 20:46:22 +0800

    used os.rename instead of file_stream

--- file_rename.py
+++ file_rename.py
@@ -7,12 +7,6 @@ parser.add_argument('infile', nargs='?', help='the name of the input file')
 parser.add_argument('outfile', nargs='?', help='the new name of the file')
 args = parser.parse_args()
 
-#Get contents of infile
-file_stream = open(args.infile, "r")
-contents = file_stream.read()
-file_stream.close()
-
-#Create the new outfile
-file_stream = open(args.outfile, "w+")
-file_stream.write(contents)
-file_stream.close()
+#Changes name of the file
+import os
+os.rename(args.infile, args.outfile)
