bfba30d8494340b4d3d6e002255f1cc2301812b5
Elcid Aaron Pangilinan <elcidaaron@gmail.com>
2019-08-22 09:35:58 +0800

    Added encoding=utf8 to file_rename.py to be html cappable

--- file_rename.py
+++ file_rename.py
@@ -8,11 +8,11 @@ parser.add_argument('outfile', nargs='?', help='the new name of the file')
 args = parser.parse_args()
 
 #Get contents of infile
-file_stream = open(args.infile, "r")
+file_stream = open(args.infile, "r", encoding="utf8")
 contents = file_stream.read()
 file_stream.close()
 
 #Create the new outfile
-file_stream = open(args.outfile, "w+")
+file_stream = open(args.outfile, "w+", encoding="utf8")
 file_stream.write(contents)
 file_stream.close()
